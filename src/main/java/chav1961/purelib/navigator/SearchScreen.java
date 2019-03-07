d50296880b10e69344b968bc0d2b8207dff1cb44
Alexander Chernomyrdin <chav1961@mail.ru>
2019-03-07 21:18:10 +0500

    DAily commit

--- src/main/java/chav1961/purelib/navigator/SearchScreen.java
+++ src/main/java/chav1961/purelib/navigator/SearchScreen.java
@@ -1,12 +1,31 @@
 package chav1961.purelib.navigator;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+import javax.swing.JTree;
+
+import org.w3c.dom.Document;
 
 public class SearchScreen extends JPanel {
 	private static final long serialVersionUID = -8655690774982200581L;
 
+	private final JTree		leftNavigator = new JTree();
+	private final JTextArea	rightContent = new JTextArea();
+	private final Document	modelContent;
+	
 	public SearchScreen() {
-		add(new JButton("Search"));
+		final JSplitPane	jsp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		
+		this.modelContent = null;
+		this.setLayout(new BorderLayout());
+		jsp.setLeftComponent(new JScrollPane(leftNavigator));
+		jsp.setRightComponent(rightContent);
+		add(jsp,BorderLayout.CENTER);
+		jsp.setDividerLocation(400);
 	}
 }
