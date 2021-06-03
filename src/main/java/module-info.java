b96ea9320ded1b47d4ca04d3624b6a8d90d5941a
chav1961 <chav1961@mail.ru>
2021-06-03 16:16:08 +0500

    Daily commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,7 @@ module chav1961.purelibnavigator {
 	requires java.datatransfer;
 
 	exports chav1961.purelibnavigator.admin; 
+	exports chav1961.purelibnavigator.test; 
 	opens chav1961.purelibnavigator.admin to chav1961.purelib; 
 	exports chav1961.purelibnavigator.javadoc; 
 }
